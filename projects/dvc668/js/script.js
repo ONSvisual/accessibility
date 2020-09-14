69dd01cf94959fd77addbdaf600f02a24f64741c
henryjameslau <henry.james.lau@gmail.com>
2020-09-14 16:32:34 +0100

    prevent default

--- projects/dvc668/js/script.js
+++ projects/dvc668/js/script.js
@@ -406,6 +406,7 @@ if (Modernizr.webgl) {
       $("#areaselect").val(code).trigger("chosen:updated");
       d3.select('abbr').on('keypress',function(evt){
 				if(d3.event.keyCode==13 || d3.event.keyCode==32){
+          d3.event.preventDefault();
 					$("#areaselect").val("").trigger('chosen:updated');
 				}
 			})
